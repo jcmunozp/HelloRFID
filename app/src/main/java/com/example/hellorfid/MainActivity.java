c978c8ffa039cce5c6608abae3e34c8bb7d324a0
Juan <jcmunozp@oesia.com>
2022-11-28 13:53:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hellorfid/MainActivity.java
@@ -0,0 +1,247 @@
+package com.example.hellorfid;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.hellorfid.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.zebra.rfid.api3.*;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    private static Readers readers;
+    private static ArrayList availableRFIDReaderList;
+    private static ReaderDevice readerDevice;
+    private static RFIDReader reader;
+    private static String TAG = "DEMO";
+    TextView textView;
+    private EventHandler eventHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        textView = (TextView) findViewById(R.id.TagText);
+        // SDK
+        if (readers == null) {
+            readers = new Readers(this, ENUM_TRANSPORT.SERVICE_SERIAL);
+        }
+
+        new AsyncTask() {
+
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                try {
+                    if (readers != null) {
+                        if (readers.GetAvailableRFIDReaderList() != null) {
+                            availableRFIDReaderList = readers.GetAvailableRFIDReaderList();
+                            if (availableRFIDReaderList.size() != 0) {
+                                // get first reader from list
+                                readerDevice = (ReaderDevice) availableRFIDReaderList.get(0);
+                                reader = readerDevice.getRFIDReader();
+                                if (!reader.isConnected()) {
+                                    // Establish connection to the RFID Reader
+                                    reader.connect();
+                                    ConfigureReader();
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                } catch (InvalidUsageException e) {
+                    e.printStackTrace();
+                } catch (OperationFailureException e) {
+                    e.printStackTrace();
+                    Log.d(TAG, "OperationFailureException " + e.getVendorMessage());
+                }
+                return false;
+            }
+
+            protected void onPostExecute(Boolean aBoolean) {
+                super.onPostExecute(aBoolean);
+                if (aBoolean) {
+                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                    textView.setText("Reader connected");
+                }
+            }
+        }.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    private void ConfigureReader() {
+        if (reader.isConnected()) {
+            TriggerInfo triggerInfo = new TriggerInfo();
+            triggerInfo.StartTrigger.setTriggerType(START_TRIGGER_TYPE.START_TRIGGER_TYPE_IMMEDIATE);
+            triggerInfo.StopTrigger.setTriggerType(STOP_TRIGGER_TYPE.STOP_TRIGGER_TYPE_IMMEDIATE);
+            try {
+                // receive events from reader
+                if (eventHandler == null)
+                    eventHandler = new EventHandler();
+                reader.Events.addEventsListener(eventHandler);
+                // HH event
+                reader.Events.setHandheldEvent(true);
+                // tag event with tag data
+                reader.Events.setTagReadEvent(true);
+                // application will collect tag using getReadTags API
+                reader.Events.setAttachTagDataWithReadEvent(false);
+                // set trigger mode as rfid so scanner beam will not come
+                reader.Config.setTriggerMode(ENUM_TRIGGER_MODE.RFID_MODE, true);
+                // set start and stop triggers
+                reader.Config.setStartTrigger(triggerInfo.StartTrigger);
+                reader.Config.setStopTrigger(triggerInfo.StopTrigger);
+            } catch (InvalidUsageException e) {
+                e.printStackTrace();
+            } catch (OperationFailureException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            if (reader != null) {
+                reader.Events.removeEventsListener(eventHandler);
+                reader.disconnect();
+                Toast.makeText(getApplicationContext(), "Disconnecting reader", Toast.LENGTH_LONG).show();
+                reader = null;
+                readers.Dispose();
+                readers = null;
+            }
+        } catch (InvalidUsageException e) {
+            e.printStackTrace();
+        } catch (OperationFailureException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Read/Status Notify handler
+    // Implement the RfidEventsLister class to receive event notifications
+    public class EventHandler implements RfidEventsListener {
+        // Read Event Notification
+        public void eventReadNotify(RfidReadEvents e) {
+            // Recommended to use new method getReadTagsEx for better performance in case of large tag population
+            TagData[] myTags = reader.Actions.getReadTags(100);
+            if (myTags != null) {
+                for (int index = 0; index < myTags.length; index++) {
+                    Log.d(TAG, "Tag ID " + myTags[index].getTagID());
+                    if (myTags[index].getOpCode() == ACCESS_OPERATION_CODE.ACCESS_OPERATION_READ &&
+                            myTags[index].getOpStatus() == ACCESS_OPERATION_STATUS.ACCESS_SUCCESS) {
+                        if (myTags[index].getMemoryBankData().length() > 0) {
+                            Log.d(TAG, " Mem Bank Data " + myTags[index].getMemoryBankData());
+                        }
+                    }
+                }
+            }
+        }
+
+        // Status Event Notification
+        public void eventStatusNotify(RfidStatusEvents rfidStatusEvents) {
+            Log.d(TAG, "Status Notification: " + rfidStatusEvents.StatusEventData.getStatusEventType());
+            if (rfidStatusEvents.StatusEventData.getStatusEventType() == STATUS_EVENT_TYPE.HANDHELD_TRIGGER_EVENT) {
+                if (rfidStatusEvents.StatusEventData.HandheldTriggerEventData.getHandheldEvent() == HANDHELD_TRIGGER_EVENT_TYPE.HANDHELD_TRIGGER_PRESSED) {
+                    new AsyncTask() {
+
+                        @Override
+                        protected Object doInBackground(Object[] objects) {
+                            try {
+                                reader.Actions.Inventory.perform();
+                            } catch (InvalidUsageException e) {
+                                e.printStackTrace();
+                            } catch (OperationFailureException e) {
+                                e.printStackTrace();
+                            }
+                            return null;
+                        }
+
+                    }.execute();
+                }
+                if (rfidStatusEvents.StatusEventData.HandheldTriggerEventData.getHandheldEvent() == HANDHELD_TRIGGER_EVENT_TYPE.HANDHELD_TRIGGER_RELEASED) {
+                    new AsyncTask() {
+
+                        @Override
+                        protected Object doInBackground(Object[] objects) {
+                            try {
+                                reader.Actions.Inventory.stop();
+                            } catch (InvalidUsageException e) {
+                                e.printStackTrace();
+                            } catch (OperationFailureException e) {
+                                e.printStackTrace();
+                            }
+                            return null;
+                        }
+
+                    }.execute();
+                }
+            }
+        }
+    }
+}
